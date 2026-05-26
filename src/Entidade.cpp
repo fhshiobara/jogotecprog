6023f798cb422d3c3069a7bd6e197e481ef95ec8
Felipe Shiobara <fhshiobara@gmail.com>
2026-05-26 18:26:56 -0300

    Delete src directory

--- src/Entidade.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entidade.hpp"
-
-using namespace Entidades;
-
-Entidade::Entidade(float Xinicial, float inicialY) : pos(Xinicial, inicialY) {}
-
-Entidade::~Entidade() {}
-
-void Entidade::salvarDataBuffer() {}
--- src/Personagem.cpp
+++
@@ -1,7 +0,0 @@
-#include "Personagem.hpp"
-
-Personagem::Personagem(float Xinicial, float inicialY, int hp) : Entidade(Xinicial, inicialY), Hp(hp) {}
-
-Personagem::~Personagem() {}
-
-void Personagem::salvarDataBuffer() {}
