eb7def07cac6c35c328bf76afbca06304b6bfdfe
Felipe Shiobara <fhshiobara@gmail.com>
2026-06-08 15:27:54 -0300

    Classe abstrata Fase

---
+++ Fase.cpp
@@ -0,0 +1,85 @@
+//
+//  Fase.cpp
+//  Jogo
+//
+//  Created by Edison Shiobara on 08/06/26.
+//
+#pragma once
+#include "Fase.hpp"
+#include "SingleFrameAnimation.hpp"
+#include "Plataforma.hpp"
+#define MAX 3
+
+
+
+namespace Fases{
+
+
+Fase::Fase():pGG(Gerenciadores::GerenciadorColisoes::getInstance()){}
+
+
+Fase::~Fase(){}
+
+void Fase::criarInimigosFaceis(){
+    //TODO
+    
+    
+}
+
+void Fase::criarPlataformas(){
+    
+    for(int i=0;i<3;i++){
+        Plataforma* pPlat = NULL;
+        pPlat = new Plataforma(CoordF(0.0f,0.0f),100.0f,20.0f); // precisamos randomizar isso depois
+        
+        if(pPlat!=NULL){
+            list_ents.incluir(pPlat);
+            pPlat->desenhar();
+            
+            
+        }
+        else{
+            std::cerr<<"ERRO: Alocacao falha de plataforma em Fase::criarPlataformas"<<std::endl;
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
+void Fase::criarCenario(){
+    Gerenciadores::SingleFrameAnimation* imagemFundo = NULL;
+    imagemFundo = new Gerenciadores::SingleFrameAnimation("../assets/background.png",CoordF(0.0f,0.0f),CoordF(800.0f,600.0f),0.79);
+    
+    imagemFundo->render();
+    
+}
+
+void Fase::criarInimigosMedios(){
+    
+    for(int i=0; i< MAX;i++){ //quantidade padrao é 3
+        Inimigo* pInim = NULL;
+        pInim = new Inimigo(CoordF(0.0f,0.0f),2,1.0f); //os parametros podem ser mudados aqui para alterar a dificuldade
+        if(pInim !=NULL){
+            list_ents.incluir(pInim);
+        }
+        else{std::cerr<<"ERRO: falha na criacao do inimigo medio"<<std::endl;}
+    }
+    
+
+}
+
+
+
+
+}
+
+
---
+++ Fase.hpp
@@ -0,0 +1,44 @@
+//
+//  Fase.hpp
+//  Jogo
+//
+//  Created by Edison Shiobara on 08/06/26.
+//
+
+#ifndef Fase_hpp
+#define Fase_hpp
+
+#pragma once
+#include "Ente.hpp"
+#include "ListaEntidades.hpp"
+#include "GerenciadorColisoes.hpp"
+namespace Fases{
+class Fase:public Ente{
+    
+protected:
+    ListaEntidades list_ents;
+    Gerenciadores::GerenciadorColisoes* pGG;
+public:
+    Fase();
+    ~Fase();
+    
+    virtual void executar() =0;
+    
+protected:
+    void criarInimigosFaceis();
+    void criarPlataformas();
+    
+    virtual void criarInimigos() =0;
+    virtual void criarObstaculos() =0;
+    void criarCenario();
+    void criarInimigosMedios();
+    
+    
+    
+    
+};
+
+
+
+#endif /* Fase_hpp */
+}
