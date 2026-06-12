f252d680fa353af693508f6d1bf65c1b98864066
Felipe Shiobara <fhshiobara@gmail.com>
2026-06-12 08:54:56 -0300

    Add sprite initialization in Obstaculo constructor

--- src/Obstaculo.cpp
+++ src/Obstaculo.cpp
@@ -3,7 +3,7 @@
 namespace Obstaculos {
 
     Obstaculo::Obstaculo(CoordF position, bool danoso)
-        : Entidade(position, CoordF(0.f, 0.f)), danoso(danoso) {}
+        : Entidade(position, CoordF(0.f, 0.f)), danoso(danoso),sprite(NULL) {}
 
     Obstaculo::~Obstaculo() {}
 
